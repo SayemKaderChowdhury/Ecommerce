f7833519b2b15454801b928300ba718ae7365cc3
SayemKaderChowdhury <sayem.kader.chowdhury@g.bracu.ac.bd>
2020-07-12 03:28:51 +0600

    Added checkout page data

--- store/views.py
+++ store/views.py
@@ -13,12 +13,12 @@ def store(request):
 
 
 def cart(request):
-
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
+        # Create empty cart for now for none-logged in users
         items = []
         order = {
             'get_cart_total': 0,
@@ -33,5 +33,20 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for none-logged in users
+        items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0
+        }
+
+    context = {
+        'items': items,
+        'order': order
+    }
     return render(request, 'store/checkout.html', context)
