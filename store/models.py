cac3a7f3349f9cc6a99af3cee15e794fecbc273f
SayemKaderChowdhury <sayem.kader.chowdhury@g.bracu.ac.bd>
2020-07-12 00:54:31 +0600

    Created image model and added defalut image route

--- store/models.py
+++ store/models.py
@@ -16,11 +16,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=5)
     digital = models.BooleanField(default=False, null=True, blank=True)
-
-    # image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
